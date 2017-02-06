eec69df797d4b041e88b6c6f63a8be981a4f0c8d
Cherkir <krischerven@gmail.com>
2017-02-05 19:31:28 -0600

    edited some stuff with aphroditebot

---
+++ isostation13/AphroditeBot/config.py
@@ -0,0 +1,9 @@
+host = "45.76.29.104"
+port = 8000
+gameport = 5000
+token = "token"
+ahelpID = "000000000000000000"
+mainID = "274310484004438018"
+triggerString = "!"
+commskey = "key"
+perm_roles = ["Administration", "Development"]
